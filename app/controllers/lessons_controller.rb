1020a6b042b4485665de72865ecae127fb0cd7f7
ngango0205 <quynhnga27231096@gmail.com>
2018-07-23 15:27:47 +0700

    Create, show lesson

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,33 @@
+class LessonsController < ApplicationController
+  def index
+    @lessons = current_user.lessons.page(params[:page])
+      .per Settings.page.child_in_page
+  end
+
+  def new
+    @lesson = Lesson.new
+  end
+
+  def create
+    @lesson = current_user.lessons.build lesson_params
+    if @lesson.save
+      redirect_to @lesson
+    else
+      flash.now[:danger] = t "create_lesson_fail"
+      render :new
+    end
+  end
+
+  def show
+    @lesson = Lesson.find_by id: params[:id]
+    return if @lesson
+    flash.now[:danger] = t "show_lesson_fail"
+    redirect_to lessons_path
+  end
+
+  private
+
+  def lesson_params
+    params.require(:lesson).permit :name, :description, :content
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :histories, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :lessons, dependent: :destroy
   has_many :follower_relations, class_name: UserRelation.name,
     foreign_key: "follower_id",
     dependent: :destroy
