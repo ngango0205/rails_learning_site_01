441b4602063c8f94940fcde10468e5949152626e
ngango0205 <quynhnga27231096@gmail.com>
2018-07-26 14:17:50 +0700

    ajax search  + edit user

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,7 @@
+//= require jquery
+//= require jquery_ujs
+//= require jquery-ui
+//= require jquery-ui/widgets/autocomplete
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,12 @@ class ApplicationController < ActionController::Base
     Notification.create(actor: current_user, recipient: recipient,
       action: t("post"), notifiable: notifiable).send_notification_email
   end
+  private
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t "flash.pls_login"
+    redirect_to login_url
+  end
 end
