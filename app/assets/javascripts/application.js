4a31ae4b902eba4e566c15624f53648169dff667
ngango0205 <quynhnga27231096@gmail.com>
2018-08-12 17:37:15 +0700

    ckeditor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,7 @@
 //= require jquery
 //= require jquery-ui/widgets/autocomplete
 //= require rails-ujs
+//= require ckeditor/init
 //= require activestorage
 //= require turbolinks
 //= require_tree .
